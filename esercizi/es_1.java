e01959c9ef9d89657d0ddb825c9aca0657456050
Nima-00 <nicola.maestri@guetti.tn.it>
2025-10-27 16:58:12 +0000

    Modified esercizi/es_1: added feature to instert costumer choice

--- esercizi/es_1.java
+++ esercizi/es_1.java
@@ -1,5 +1,13 @@
+/* Exercise 1: Restaurant chatbot */
+
+import java.util.Scanner;
+
 public class es_1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("What would you like to eat for lunch?");
+        String food = scanner.nextLine();
+        System.out.println("You chose: " + food);
+        scanner.close();
     }
 }
